5dc102c4a9366ea1ed20eb9ee2177bae3887df02
Rafael Venancio <rafael.venancio08.06@gmail.com>
2024-06-09 12:23:36 -0300

    Done pyproject, docker-compose, BD connection and BD models

---
+++ database/connection.py
@@ -0,0 +1,8 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "postgresql+asyncpg://admin:admin@localhost:5432/cripto"
+
+engine = create_async_engine(DATABASE_URL)
+async_session = sessionmaker(engine, class_=AsyncSession)
---
+++ projeto_final_pweb/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, APIRouter
+
+
+app = FastAPI()
+router = APIRouter()
+
+@router.get('/')
+def first():
+    return 'Hello world!'
+
+app.include_router(prefix='/first',router=router)
+
